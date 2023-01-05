ed9e9cb9b0e216aac5b9ee66d2194f7960ae2170
sanghun <sse.sf12go@gmail.com>
2023-01-05 11:41:17 +0900

    초기 커밋.

---
+++ autocad02/Commands.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+namespace autocad02
+{
+    public class Commands
+    {
+        /// <summary>
+        /// 라인을 그리기 샘플
+        /// </summary>
+        [CommandMethod("DLC")]
+        public static void DrawLineCurve()
+        {
+            var document = Application.DocumentManager.MdiActiveDocument;
+            var database = document.Database;
+            using (var transaction = database.TransactionManager.StartTransaction())
+            {
+                var blockTableRecord = (BlockTableRecord)transaction.GetObject(database.CurrentSpaceId, OpenMode.ForWrite);
+                var line = new Line(new Point3d(0, 0, 0), new Point3d(100, 100, 0));
+                blockTableRecord.AppendEntity(line);
+                transaction.Commit();
+            }
+        }
+    }
+}
